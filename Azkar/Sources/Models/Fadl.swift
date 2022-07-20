5de71a378916573b08d8543c87f5da65e6e9ad7c
avoc0der <avoc0der@pm.me>
2022-07-20 23:47:41 +0300

    Set english as default language

--- Azkar/Sources/Models/Fadl.swift
+++ Azkar/Sources/Models/Fadl.swift
@@ -13,21 +13,21 @@ struct Fadl: Codable, Identifiable {
     enum CodingKeys: String, CodingKey {
         case id
         case _source = "source", sourceExtension = "source_ext"
-        case _text = "text", textEn = "text_en", textAr = "text_ar"
+        case _text = "text", textRu = "text_ru", textAr = "text_ar"
     }
     
     let id: Int
     private let _text: String
-    private let textEn: String?
+    private let textRu: String?
     private let textAr: String?
     private let _source: String
     private let sourceExtension: String?
     
     var text: String {
         switch languageIdentifier {
-        case .ru: return _text
+        case .ru: return textRu ?? _text
         case .ar: return textAr ?? _text
-        default: return textEn ?? _text
+        default: return _text
         }
     }
     
