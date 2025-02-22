abdc5e74601ecd46d9e75978dec214817168f727
avocoder <avoc0der@pm.me>
2025-02-23 00:59:32 +0200

    Link Superwall as static library

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
     import ProjectDescription
 
     let packageSettings = PackageSettings(
+        productTypes: [
+            "SuperwallKit": .staticLibrary,
+        ]
     )
 #endif
 
